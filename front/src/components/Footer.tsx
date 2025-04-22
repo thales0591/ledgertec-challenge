80f83e679bed7824cda7ec1a7e4278af7eb2708e
thales0591 <thales0591@gmail.com>
2025-04-22 12:27:34 -0300

    front/feat: add my social media links to footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,7 +1,32 @@
+import githubIcon from '../assets/githubIcon.png'
+import whatsappIcon from '../assets/whatsappIcon.png'
+import emailIcon from '../assets/emailIcon.png'
+
 export function Footer() {
   return (
     <footer className="bg-[#60787e] h-8 flex justify-center items-center">
-      <h3 className="font-medium">Thales Oliveira</h3>
+      <h3 className="font-medium">Candidato Thales Oliveira</h3>
+      <a
+        href="https://github.com/thales0591"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={githubIcon} alt="GitHub" className="h-5 ml-4" />
+      </a>
+      <a
+        href="https://wa.me/5583999700025"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={whatsappIcon} alt="GitHub" className="h-5 ml-4" />
+      </a>
+      <a
+        href="mailto:thales0591@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={emailIcon} alt="GitHub" className="h-5 ml-4" />
+      </a>
     </footer>
   )
 }
